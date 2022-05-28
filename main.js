0e42bedc5cdcb2ed08c1ca13050e1b2a319d1f9d
Sylvia <sylvia@donchev.is>
2022-05-28 20:47:31 +0200

    favoritemovies string

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
-const express = require('express'),
-bodyParser = require('body-parser'),
-uuid = require('uuid');
-
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 
 const app = express();
 app.use(bodyParser.json());
 
-let students - [
+let students = [
   {
     id: 1,
     name: "Jessica Drake",
     classes: {
       biology: 95,
-      algebra: 92
-    }
+      algebra: 92,
+    },
   },
   {
     id: 2,
     name: "Ben Cohen",
     classes: {
       biology: 95,
-      algebra: 92
-    }
+      algebra: 92,
+    },
   },
   {
     id: 3,
     name: "Lisa Downing",
     classes: {
       biology: 95,
-      algebra: 92
-    }
-  }
+      algebra: 92,
+    },
+  },
 ];
 
 app.get("/students/:name", (req, res) => {
-  res.json(students.find((student) => 
-  { return student.name === req.params.name }));
+  res.json(
+    students.find((student) => {
+      return student.name === req.params.name;
+    })
+  );
 });
 
 app.post("/students", (req, res) => {
@@ -52,69 +54,90 @@ app.post("/students", (req, res) => {
 });
 
 app.delete("/students/:id", (req, res) => {
-  let student = students.find((student) => { return student.id === req.params.id
+  let student = students.find((student) => {
+    return student.id === req.params.id;
   });
 
   if (student) {
-    students = students.filter((obj) => { return obj.id !== req.params.id });
+    students = students.filter((obj) => {
+      return obj.id !== req.params.id;
+    });
     res.status(201).send("Student " + req.params.id + "was deleted.");
-    }
+  }
+});
+
+app.put("/students/:name/:class/:grade", (req, res) => {
+  let student = students.find((student) => {
+    return student.name === req.params.name;
+  });
+  if (student) {
+    student.classes[req.params.class] = parseInt(rew.params.grade);
+    res
+      .status(201)
+      .send(
+        "Student " +
+          req.params.name +
+          "was assigned a grade of " +
+          req.params.grade +
+          " in " +
+          req.params.class
+      );
+  } else {
+    res
+      .status(404)
+      .send("Student with the name " + req.params.name + " was not found.");
+  }
+});
+
+app.get("Students/:name/gpa", (req, res) => {
+  let student = students.find((student) => {
+    return student.name === req.params.name;
   });
 
-  app.put("/students/:name/:class/:grade", (req, res) => {
-    let student = students.find((student) => { return student.name ===
-    req.params.name });
-    if (student) {
-      student.classes[req.params.class] = parseInt(rew.params.grade);
-      res.status(201).send("Student " + req.params.name + "was assigned a grade of " + req.params.grade + " in " + req.params.class);
-    } else {
-    res.status(404).send("Student with the name " + req.params.name + " was not found.");
-    }
-  });
+  if (student) {
+    let classesGrades = Object.values(student.classes);
+    let sumOfGrades = 0;
+    classesGrades.forEach((grade) => {
+      sumOfGrades = sumOfGrades + grade;
+    });
+    let gpa = sumOfGrades / classesGrades.length;
+    console.log(sumOfGrades);
+    console.log(classesGrades.length);
+    console.log(gpa);
+    res.status(2021).send("" + gpa);
+  } else {
+    res
+      .status(404)
+      .send("Student with the name " + req.params.name + " was not found.");
+  }
+});
 
-  app.get("Students/:name/gpa", (req, res) => {
-    let student = students.find((student) => { return student.name === req.params.name });
+app.listen(8080, () => {
+  console.log("Your app is listening in port 8080");
+});
 
-    if (student) {
-      let classesGrades = Object.values(student.classes);
-      let sumOfGrades = 0;
-      classesGrades.forEach(grade => {
-        sumOfGrades = sumOfGrades + grade;
-      });
-   let gpa = sumOfGrades / classesGrades.length;
-   console.log(sumOfGrades);
-   console.log(classesGrades.length);
-   console.log(gpa);
-   res.status(2021).send("" + gpa);
-    } else {
-      res.status(404).send("Student with the name " + req.params.name + " was not found.");
-    }
-  });
+app.get("/Students", (req, res) => {
+  res.json(students);
+});
 
-  app.listen(8080, () => {
-    console.log("Your app is listening in port 8080");
-  });
+app.post("/students", (req, res) => {
+  let newStudent = req.body;
 
-  app.get("/Students", (req, res) => {
-    res.json(students);
-  });
-
-  app.post("/students", (req, res) => {
-    let newStudent = req.body;
-    
-    if (!newStudent.name) {
-      const message = `Missing "name" 
+  if (!newStudent.name) {
+    const message = `Missing "name" 
       in request body`;
-res.status(400).send(message);
-    } else {
-      newStudent.id = uuid.v4();
-      students.push(newStudent);
-      res.status(201).send(newStudent);
-    }
-  });
+    res.status(400).send(message);
+  } else {
+    newStudent.id = uuid.v4();
+    students.push(newStudent);
+    res.status(201).send(newStudent);
+  }
+});
 
-  app.get("/students/:name", (req, res) => {
-    res.json(students.find( (student) =>
-    { return student.name === req.params.name }));
-  });
-
+app.get("/students/:name", (req, res) => {
+  res.json(
+    students.find((student) => {
+      return student.name === req.params.name;
+    })
+  );
+});
